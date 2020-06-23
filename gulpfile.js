e08a429350024ed440935a4beed1c806fc3fd5bc
Alex <alexbelka1992@mail.ru>
2020-06-23 21:45:02 +0300

    Верстка и стили страницы index

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+"use strict"
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const sourcemaps = require('gulp-sourcemaps');
+const server = require('browser-sync').create();
+const prefixer = require('gulp-autoprefixer');
+const {series} = require('gulp');
+const rename = require('gulp-rename');
+const minJs = require('gulp-uglify');
+const imageMin = require('gulp-imagemin');
+const svgMin =require('gulp-svgmin');
+const jpgMin =require('gulp-jpeg-2000');
+
+function Sass(){
+    return gulp.src(['src/scss/style.scss'],{base:'scss'})
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(prefixer())
+        .pipe(sourcemaps.write())
+        .pipe(rename('style.css'))
+        .pipe(gulp.dest('build/css'))
+        .pipe(server.stream())
+}
+
+function Js() {
+    return gulp.src(['src/js/*.js'], {base: 'src'})
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(minJs())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('build/js'))
+}
+
+function Images(){
+    return gulp.src(['src/images/*'])
+        .pipe(imageMin())
+        .pipe(gulp.dest('build/images'))
+}
+
+function Svg(){
+    return gulp.src(['src/svg/*'])
+        .pipe(svgMin())
+        .pipe(gulp.dest('build/svg'))
+}
+
+function Jpeg(){
+    return gulp.src(['src/jpg/*'])
+        .pipe(imageMin())
+        .pipe(gulp.dest('build/jpg'))
+}
+
+function Server(){
+    server.init({server:"./",browser:'firefox'});
+    gulp.watch('html/*.html').on('change', server.reload)
+    gulp.watch('src/images').on('change',server.reload)
+    gulp.watch('src/scss/**/*.scss',gulp.series(Sass,Js,Images,Svg,Jpeg));
+}
+
+exports.start = series(Sass,Js,Svg,Images,Jpeg,Server);
